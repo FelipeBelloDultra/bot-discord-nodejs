94586b300f9fe6866c4050d5d4002001b1199dc2
Felipe Bello Dultra <felipe_bello_dultra@hotmail.com>
2020-10-04 13:09:01 -0300

    feat: add background black image on the image

--- src/modules/bot/services/GenerateImageWithTextService.js
+++ src/modules/bot/services/GenerateImageWithTextService.js
@@ -6,6 +6,17 @@ const randomPhoto = require('../../../utils/randomPhoto');
 class GenerateImageWithTextService {
   async execute(message) {
     const max = randomPhoto.length;
+
+    const background = path.resolve(
+      __dirname,
+      '..',
+      '..',
+      '..',
+      'utils',
+      'img',
+      'background.jpg',
+    );
+
     const writePath = path.resolve(
       __dirname,
       '..',
@@ -18,40 +29,17 @@ class GenerateImageWithTextService {
 
     const photo = Math.floor(Math.random() * max);
 
-    const fontBlack = await jimp.loadFont(jimp.FONT_SANS_32_BLACK);
     const fontWhite = await jimp.loadFont(jimp.FONT_SANS_32_WHITE);
+
     const image = await jimp.read(randomPhoto[photo]);
+    const backgroundImage = await jimp.read(background);
 
-    const resizeImage = image.resize(500, 500);
+    image.resize(600, 600);
+    backgroundImage.resize(600, 700);
 
-    // Text down
-    if (photo === 0 || photo === 7) {
-      resizeImage.print(fontBlack, 10, 450, message, 400).write(writePath);
+    backgroundImage.print(fontWhite, 10, 10, message, 550);
 
-      return writePath;
-    }
-
-    // Text up and white
-    if (photo === 3 || photo === 6 || photo === 9 || photo === 15) {
-      resizeImage.print(fontWhite, 10, 10, message, 400).write(writePath);
-
-      return writePath;
-    }
-
-    // Text down and white
-    if (
-      photo === 12 ||
-      photo === 13 ||
-      photo === 14 ||
-      photo === 16 ||
-      photo === 18
-    ) {
-      resizeImage.print(fontWhite, 10, 450, message, 400).write(writePath);
-
-      return writePath;
-    }
-
-    resizeImage.print(fontBlack, 10, 10, message, 400).write(writePath);
+    backgroundImage.composite(image, 0, 100).write(writePath);
 
     return writePath;
   }
